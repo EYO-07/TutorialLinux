6621be1bd143d937728cc762c0519e27ef5bd591
oZumbiAnalitico <52110875+EYO-07@users.noreply.github.com>
2026-05-31 09:24:53 -0300

    Add files via upload

--- ArtigoDebianMinimo/DEB_install.sh
+++ ArtigoDebianMinimo/DEB_install.sh
@@ -1,46 +1,96 @@
 # BEGIN 
 
 # Installation Script for Minimal Debian Install (i3+picom)
-# ... install after installation of sudo command 
-# ... comment-out if you don't want a specific package 
+# 1. install after installation of sudo command
+# ... add the user to group sudo and login with ordinary sudo user
+# 2. uncomment or comment-out if you don't want a specific package 
+# sudo apt install -y #
+
 # -- update package list and upgrade 
 sudo apt update && sudo apt upgrade
+
 # -- internet 
-sudo apt install network-manager # command-line network interface 
+sudo apt install -y network-manager net-tools # command-line network interface 
+# sudo apt install -y nethogs # internet terminal application watcher 
+
 # -- firmware 
-sudo apt install firmware-linux firmware-linux-nonfree firmware-misc-nonfree # firmware, drivers 
-# sudo apt install linux-headers-$(uname -r) nvidia-detect # if you have nvidia graphic-card 
+sudo apt install -y firmware-linux firmware-linux-nonfree firmware-misc-nonfree # firmware, drivers 
+# sudo apt install -y linux-headers-$(uname -r) nvidia-detect # if you have nvidia graphic-card 
+
 # -- graphical server and window manager 
-sudo apt install xorg dbus-x11 # x11 graphical server 
-sudo apt install i3 picom # i3 window manager and picom compositor 
-# sudo apt install lightdm lightdm-gtk-greeter # graphical login 
-# -- build/compilation toolchain 
-sudo apt install build-essential # install GCC + make
-sudo apt install libx11-dev libxkbcommon-dev libxkbcommon-x11-dev # x11 dev tools 
+sudo apt install -y xorg dbus-x11 # x11 graphical server 
+sudo apt install -y i3 picom # i3 window manager and picom compositor 
+# sudo apt install -y lightdm lightdm-gtk-greeter # graphical login 
+
+# -- build/compilation toolchain/toolkits 
+sudo apt install -y build-essential # install GCC + make
+sudo apt install -y libx11-dev libxkbcommon-dev libxkbcommon-x11-dev # x11 dev tools 
+sudo apt install -y libgtk-3-0 libgtk-4-1 # gtk toolkit for gtk graphical interface apps 
+
 # -- sound
-sudo apt install pipewire pipewire-pulse pipewire-alsa pipewire-jack # sound related apps 
-sudo apt install wireplumber # sound server 
+sudo apt install -y pipewire pipewire-pulse pipewire-alsa pipewire-jack # sound related apps 
+sudo apt install -y wireplumber # sound server 
+
 # -- apps 
-sudo apt install libgtk-3-0 libgtk-4-1 # gtk toolkit for gtk graphical interface apps 
-sudo apt install thunar lxpolkit lxappearance lxrandr arandr # some basic gtk apps 
-sudo apt install 7zip # extract compression tool 
-sudo apt install firefox-esr # firefox to install brave
-sudo apt install curl wget git # tools to get things on internet 
-sudo apt install htop # taskmanager 
-sudo apt install pavucontrol alsa-utils # guis for sound configs 
-# sudo apt install btop # better taskmanager
-sudo apt install feh nitrogen scrot # screenshot and background tools 
+sudo apt install -y thunar # gtk file explorer
+sudo apt install -y lxpolkit # gtk daemon for sudo privilege scalation
+# sudo apt install -y polkitd # daemon for sudo privilege scalation
+sudo apt install -y lxappearance # gtk appearance
+sudo apt install -y lxrandr # gtk to set display monitors
+# sudo apt install -y arandr # gtk to set display monitors (legacy,older)
+sudo apt install -y 7zip # file extract/compression tool 
+sudo apt install -y firefox-esr # firefox to install brave (firefox is the new internet explorer :3)
+sudo apt install -y curl wget git # tools to get things on internet 
+sudo apt install -y htop # taskmanager 
+# sudo apt install -y btop # better taskmanager
+sudo apt install -y pavucontrol alsa-utils # guis for sound configs 
+sudo apt install -y feh nitrogen scrot imagemagick # screenshot and background tools 
+# sudo apt install -y geany # light text/code editor
+# sudo apt install -y gnome-calendar # calendar 
+# sudo apt install -y obs-studio # video capture and video stream
+# sudo apt install -y wine wine64 winetricks # wine windows application layer
+# sudo apt install -y wine32:i386 # legacy systems wine layer
+
+# -- gtk themes 
+# sudo apt install -y adwaita-icon-theme # icon theme 
+# sudo apt install -y hicolor-icon-theme # icon theme 
+# sudo apt install -y papirus-icon-theme # icon theme 
+# sudo apt install -y gnome-themes-extra # theme 
+
 # -- system utilities
-sudo apt install gparted # partition tool
-sudo apt install xbacklight # screen brightness 
-# sudo apt install brightnessctl # screen brightness
-# sudo apt install dunst # notification daemon 
+# sudo apt install -y gparted # partition tool
+# sudo apt install -y bleachbit # ccleaner equivalent, tool to cleandisk 
+# sudo apt install -y xbacklight # screen brightness 
+sudo apt install -y brightnessctl # screen brightness
+# sudo apt install -y dunst # notification daemon 
+
 # -- fonts 
-sudo apt install fonts-dejavu-core fonts-freefont-ttf fonts-liberation \
+sudo apt install -y fonts-dejavu-core fonts-freefont-ttf fonts-liberation \
                  fonts-noto-core fonts-noto-extra fonts-noto-color-emoji \
                  fonts-noto-cjk fonts-wqy-zenhei fonts-wqy-microhei \
                  fonts-mathjax fonts-courier fonts-roboto fonts-open-sans \
+                 fonts-aenigma \
+                 fonts-cantarell \
+                 fonts-crosextra-caladea fonts-crosextra-carlito \
+                 fonts-font-awesome \
+                 fonts-symbola fonts-unifont \
                  fonts-lato fonts-msttcore
 fc-cache -fv
 
 # END
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
